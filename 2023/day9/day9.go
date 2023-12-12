a476b445b9ace3c28193e920251a52dccc61d72c
Cristhian Peverelli <cristhian.peverelli@studenti.unimi.it>
2023-12-12 09:05:07 +0100

    day9 part 1

---
+++ 2023/day9/day9.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+	filePath := "C:\\Users\\cpeverelli\\Desktop\\adventofcode\\2023\\day9\\input.txt"
+
+	// PART 1
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	for scanner.Scan() {
+		str := scanner.Text()
+		vals := strings.Split(str, " ")
+		values := []int{}
+		for _, v := range vals {
+			n, _ := strconv.Atoi(v)
+			values = append(values, n)
+
+		}
+		sum += getValue(values)
+	}
+	fmt.Println("PART 1: ", sum)
+	// fmt.Println("PART 2: ", sum)
+	fmt.Printf("Execution time: %s\n", time.Since(startTime))
+}
+
+func getValue(values []int) int {
+	if len(values) == 0 {
+		return 0
+	}
+	return values[len(values)-1] + getValue(convertedValues(values))
+}
+
+func convertedValues(values []int) []int {
+	res := []int{}
+	for i := 0; i < len(values)-1; i++ {
+		res = append(res, values[i+1]-values[i])
+	}
+	return res
+}
